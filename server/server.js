4410e4bb788b89155460f163af10c18c11d9a9fd
colin <colinwitkamp@gmail.com>
2016-11-18 11:08:17 +0100

    fix: host url

--- server/server.js
+++ server/server.js
@@ -177,7 +177,7 @@ app.use((err, req, res, next) =>	{	// eslint-disable-line no-unused-vars
 });
 
 // start app
-app.listen('0.0.0.0', serverConfig.port, (error) => {
+app.listen(serverConfig.port, '0.0.0.0', (error) => {
   if (!error) {
     console.log(`MERN is running on port: ${serverConfig.port}! Build something amazing!`); // eslint-disable-line
   }
